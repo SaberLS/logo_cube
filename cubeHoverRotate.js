90fb9b68c92380652e01e6d411f7bc7a98e49c62
SaberLS <patryk.platek19@gmail.com>
2024-11-22 17:15:16 +0100

    comments

--- cubeHoverRotate.js
+++ cubeHoverRotate.js
@@ -6,52 +6,51 @@ class Cube {
     this.cube.addEventListener(
       'animationcancel',
       () => {
-        this.cube.style.transform = '';
-        console.log('animationcancel');
+        // console.log('animationcancel');
+        this.cube.style.transform = ''; // set transform to default value. Starts transition which reverses the .rotate animation
       }
     );
 
     this.cube.addEventListener(
       'mouseover',
-      this.stopOnHoover
+      this.stop
     );
 
     this.cube.addEventListener(
       'mouseout',
-      this.rotateOnOut
+      this.rotate
     );
   }
 
-  rotate () {
-    console.log('rotate');
-    this.cube.classList.add('rotate');
-  }
+  rotate = ({'relatedTarget': movedTo}) => {
+    // console.log('mouse out');
+    // console.log(movedTo);
+
+    // if cursor was moved out of windo or on element which is not part of cube
+    if (movedTo === null || !movedTo.classList.contains('face')) {
+      // console.log('rotate');
+      this.cube.style.transform = ''; // set transform to default value
+      this.cube.classList.add('rotate'); // start rotate animation
+    }
+  };
 
   stop = () => {
-    console.log('stop');
+    // console.log('hoover');
 
-    this.cube.style.transform = this.getComputedTranform();
 
-    this.cube.classList.remove('rotate');
-  };
+    if (this.isRotating()) {
+      // console.log('stop');
+      // 1. set transform to current computed value to stop at current animation state
+      this.cube.style.transform = this.getComputedTranform();
 
-  stopOnHoover = () => {
-    console.log('hoover');
-
-    if (this.cube.classList.contains('rotate')) {
-      this.stop();
+      // 2. cancel animation
+      this.cube.classList.remove('rotate');
     }
   };
 
-  rotateOnOut = ({'relatedTarget': movedTo}) => {
-    console.log('out');
-
-    console.log(movedTo);
-    if (movedTo === null || !movedTo.classList.contains('face')) {
-      this.cube.style.transform = '';
-      this.rotate();
-    }
-  };
+  isRotating () {
+    return this.cube.classList.contains('rotate');
+  }
 
   getComputedTranform () {
     return window.getComputedStyle(this.cube).transform;
